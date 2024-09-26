5ed0dafcaa732c5a82948c256142e2069a3e237d
Soleil Walker <149134310+sunnysoleil@users.noreply.github.com>
2024-09-25 20:09:14 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const phraseUl = document.querySelector('#phrase ul');
+let scoreboard = document.querySelector('#scoreboard ol');
 
 const startBtn = document.querySelector('.btn__reset');
 
@@ -14,6 +15,7 @@ const phrases = [
     'PENNY FOR YOUR THOUGHTS'
 ]
 
+let getPhrase = '';
 
 // Return random phrase from phrases array.
 function getRandomPhraseAsArray() {
@@ -23,13 +25,13 @@ function getRandomPhraseAsArray() {
 
 //Return a random phrase and add to HTML.
 function addPhraseToDisplay() {
-    let getPhrase = getRandomPhraseAsArray();
+    getPhrase += getRandomPhraseAsArray();
     let splitPhrase = getPhrase.split('');
     const phraseUl = document.querySelector('#phrase ul');
     for (let i = 0; i < splitPhrase.length; i++) {
-        let le = splitPhrase[i];
+        let eachLetter = splitPhrase[i];
         let createLi = document.createElement('li');
-        createLi.textContent = le;
+        createLi.textContent = eachLetter;
         phraseUl.appendChild(createLi);
         if (splitPhrase[i].trim() === '') {
             createLi.className = 'space';
@@ -40,9 +42,51 @@ function addPhraseToDisplay() {
     return phraseUl;
 }
 
+function checkLetter(buttonKey) {
+    let eachLi = document.querySelectorAll('#phrase li');
+    // let getPhraseLower = getPhrase.toLowerCase();
+
+    let match = '';
+
+    for (let i = 0; i < eachLi.length; i++) {
+        let eachLiLetter = eachLi[i].textContent.toLowerCase();
+
+        if (eachLiLetter.includes(buttonKey)) {
+            eachLi[i].classList.add('show');
+            match += buttonKey;
+        }
+    }
+
+    // if (!getPhraseLower.includes(buttonKey)) {
+    //     match += 'false';
+    // } 
+
+    return match;
+}
+
 // Button to start the game.
 startBtn.addEventListener('click', () => {
     const startOverlay = document.getElementById('overlay');
     startOverlay.style.display = 'none';
     addPhraseToDisplay();
+});
+
+// On screen keyboard listener.
+qwerty.addEventListener('click', (e) => {
+    let key = e.target;
+    let keyTag = key.tagName;
+    let keyLetter = key.textContent;
+    let keyClass = key.className;
+
+    if (keyTag === 'BUTTON' && keyClass !== 'chosen'){
+        key.className = 'chosen';
+        let checkFunc = checkLetter(keyLetter);
+        let scoreboardHearts = scoreboard.lastElementChild;
+
+        // if (checkFunc === 'false') {
+        if (checkFunc === '') {
+            missed++;
+            scoreboardHearts.remove();
+        }
+    }
 });
